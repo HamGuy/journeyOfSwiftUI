11efc54a78d9edf3cd85f1f34489fae62809d79c
wangrui <wangrui15@gmail.com>
2021-09-22 16:40:01 +0800

    Initial Commit

---
+++ journeyOfSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  journeyOfSwiftUI
+//
+//  Created by HamGuy on 2021/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ journeyOfSwiftUI/journeyOfSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  journeyOfSwiftUIApp.swift
+//  journeyOfSwiftUI
+//
+//  Created by HamGuy on 2021/9/22.
+//
+
+import SwiftUI
+
+@main
+struct journeyOfSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
